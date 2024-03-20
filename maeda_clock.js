78b1f6ed36a60f8a2a31b06e0dea2a3548750684
raveqru <131623439+raveqru@users.noreply.github.com>
2024-03-20 21:15:43 +1300

    Update maeda_clock.js
    Maeda Clock

--- maeda_clock.js
+++ maeda_clock.js
@@ -1,9 +1,33 @@
-// Update this function to draw you own maeda clock on a 960x500 canvas
+let angle = 0; // Initial angle
+let radius = 200; // Radius of the circular path
+
+// Update this function to draw your own Maeda clock on a 960x500 canvas
 function draw_clock(obj) {
-  // YOUR MAIN CLOCK CODE GOES HERE
-  background(50); //  beige
-  fill(200); // dark grey
-  textSize(40);
+  background(124, 140, 209); // Set background color
+
+  //Maps
+  let hours = obj.hours; 
+  let minutes = obj.minutes; 
+  let seconds = obj.seconds; 
+
+  fill(124, 140, 209); // White fill color
+  strokeWeight(10)
+  stroke(255); // No outline
+
+  // Calculate position based on angle
+  let x = 960 / 2 + cos(angle) * radius;
+  let y = 500 / 2 + sin(angle) * radius;
+
+  //clock
+  textSize(100);
   textAlign(CENTER, CENTER);
-  text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
+  textFont("Helvetica");
+  text(nf(hours, 2) + ':' + nf(minutes, 2) + ':' + nf(seconds, 2), x, y);
+
+  // Icreasing angle for the next frame
+  angle += 0.01; // Adjust the speed of rotation as needed
+  // Resets angle when < 360 degrees
+  if (angle > TWO_PI) {
+    angle = 0;
+  }
 }
